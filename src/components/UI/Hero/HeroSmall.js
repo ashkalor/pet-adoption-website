625fc38d2c34796357a80ce1e4124db9f5f58061
AshKalor <akhibkalnoor@gmail.com>
2021-07-17 20:21:30 +0530

    Add HeroSmall UI component

---
+++ src/components/UI/Hero/HeroSmall.js
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+import { HiOutlineChevronRight } from "react-icons/hi";
+
+const HeroSmall = (props) => {
+  const { heroImg, title, page } = props;
+  return (
+    <diV className="relative w-screen h-96 bg-black">
+      <img
+        src={heroImg}
+        alt=""
+        className="w-screen h-96 object-center object-cover opacity-40"
+      />
+      <div
+        className="absolute text-5xl font-semibold text-white "
+        style={{ bottom: "30%", left: "10%" }}
+      >
+        {title}
+      </div>
+      <div
+        className="absolute rounded-md inline-flex gap-5 items-center justify-center w-52 h-9 bg-purple-700 "
+        style={{ bottom: "16%", left: "10%" }}
+      >
+        <Link
+          to="/home"
+          className=" text-md font-roboto font-light text-white hover:text-gray-300 "
+        >
+          Home
+        </Link>
+
+        <div>
+          <HiOutlineChevronRight
+            color="white"
+            size="0.7rem"
+            className="origin-center"
+          />
+        </div>
+
+        <Link
+          to={`/${page}`}
+          className=" text-md font-roboto font-light text-gray-300 hover:text-gray-400 "
+        >
+          {page}
+        </Link>
+      </div>
+    </diV>
+  );
+};
+export default HeroSmall;
