53da03047cdf9bcc91f9d222ed6288e1fc30c4b4
AshKalor <akhibkalnoor@gmail.com>
2021-07-18 23:51:04 +0530

    Add card to contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,11 @@
 import HeroSmall from "../components/UI/Hero/HeroSmall";
 import contactCover from "../assets/img/contact/contactCover.jpg";
+import Card from "../components/Adopt/AdoptCard";
 const Contact = () => {
   return (
     <>
       <HeroSmall title="Contact" heroImg={contactCover} />
+      <Card />
     </>
   );
 };
