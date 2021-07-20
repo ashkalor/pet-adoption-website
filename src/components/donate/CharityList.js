2ee6fcd20bf1a93d2a8190e73ecfe83c25c7ec0f
AshKalor <akhibkalnoor@gmail.com>
2021-07-20 23:16:16 +0530

    Add charity items in donate

---
+++ src/components/donate/CharityList.js
@@ -0,0 +1,24 @@
+import logo1 from "../../assets/img/donate/logo1.jpg";
+import logo2 from "../../assets/img/donate/logo2.jpg";
+import logo3 from "../../assets/img/donate/logo3.jpg";
+
+export const CharityList = [
+  {
+    logo: logo1,
+    title: "Stray Relief and Animal Welfare",
+    url: "www.strawindia.org",
+    donateLink: "www.strawindia.org/donate.aspx",
+  },
+  {
+    logo: logo2,
+    title: "People for Animals Bangalore",
+    url: "https://peopleforanimalsbangalore.org",
+    donateLink: "https://peopleforanimalsbangalore.org/donate",
+  },
+  {
+    logo: logo3,
+    title: "Karuna Animal Welfare ",
+    url: "https://karunaanimalwelfare.org",
+    donateLink: "https://karunaanimalwelfare.org/pet-adoption/",
+  },
+];
--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -1,9 +1,38 @@
 import HeroSmall from "../components/UI/Hero/HeroSmall";
 import contactCover from "../assets/img/contact/contactCover.jpg";
+import Card from "../components/UI/Card/Card";
+import { CharityList } from "../components/donate/CharityList";
+import Button from "../components/UI/Button/Button";
+
 const Donate = () => {
   return (
     <>
       <HeroSmall title="Donate" heroImg={contactCover} />
+      <div>
+        <Card className="p-12">
+          <div className="text-4xl font-semibold ">Pet Charity Links</div>
+          <div>
+            {CharityList.map((item) => {
+              return (
+                <div className="flex items-center">
+                  <div className="overflow-hidden h-36 w-36 ">
+                    <img
+                      src={item.logo}
+                      alt="Charity Logo"
+                      className=" object-cover object-top h-36 w-36 rounded-full"
+                    />
+                  </div>
+                  <div>
+                    <p>{item.title}</p>
+                    <p>{item.url}</p>
+                    <Button>Donate</Button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </Card>
+      </div>
     </>
   );
 };
