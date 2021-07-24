7ce9b32e2dd6c8a3f01e2fab77054f8c7d2ea9e3
AshKalor <akhibkalnoor@gmail.com>
2021-07-24 23:38:36 +0530

    Add dummy Shelter Data

---
+++ src/components/shelter/ShelterData.js
@@ -0,0 +1,45 @@
+export const ShelterData = [
+  {
+    id: 1,
+    name: "Charlie’s Animal Rescue Centre",
+    contact: "09035999372",
+    address:
+      "Survey No. 124/1, Mittaganahalli Cross, Kogilu, Near Reva College, Yelahanka.",
+    state: "Karnataka",
+    city: "Bangalore",
+    pin: "560063",
+    timings:
+      " Mondays-Saturdays: 11:00 AM to 4:00 PM,Sundays: 11:00 AM to 3:00 PM",
+  },
+  {
+    id: 2,
+    name: "Karuna Animal Shelter",
+    contact: "08023411181",
+    address: "Veterinary College Campus, Hebbal.",
+    state: "Karnataka",
+    city: "Bangalore",
+    pin: "560024",
+    timings: "Mondays - Sundays, 9:30 AM to 4:30 PM",
+  },
+  {
+    id: 3,
+    name: "The Voice of Stray Dogs",
+    contact: "info@strays.in",
+    address:
+      "1st Floor, Pine Valley Building, Embassy Golf Links, Off Intermediate Ring Road.",
+    state: "Karnataka",
+    city: "Bangalore",
+    pin: "560071",
+    timings: "Mondays-Saturdays: 9:00 AM to 6:00 PM",
+  },
+  {
+    id: 4,
+    name: "Animal Rights Fund",
+    contact: "09845009681",
+    address: "Kumarakrupa Road, Gandhi Nagar",
+    state: "Karnataka",
+    city: "Bangalore",
+    pin: "560009",
+    timings: "Mondays-Saturdays: 9:00 AM to 5:00 PM",
+  },
+];
