e002251ca237e65592d33ae63b315928a25bfd93
AshKalor <akhibkalnoor@gmail.com>
2021-08-18 00:14:13 +0530

    Add database query for donate page

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -1,7 +1,6 @@
 import HeroSmall from "../components/UI/Hero/HeroSmall";
 import contactCover from "../assets/img/contact/contactCover.jpg";
 import Card from "../components/UI/Card/Card";
-import { CharityList } from "../components/donate/CharityList";
 import Button from "../components/UI/Button/Button";
 import { useEffect, useState } from "react";
 import CharityItem from "../components/donate/CharityItem";
@@ -14,13 +13,25 @@ const Donate = () => {
   const [isNextDisabled, setIsNextDisabled] = useState(false);
   const [isPrevDisabled, setIsPrevDisabled] = useState(false);
 
+  const [donateData, setDonateData] = useState([]);
+
   useEffect(() => {
-    setTotalPages(Math.round(CharityList.length / itemsPerPage));
+    const db = firebase.firestore();
+    db.collection("Donate")
+      .get()
+      .then((querySnapshot) => {
+        const documents = querySnapshot.docs.map((doc) => doc.data());
+        setDonateData(documents);
+      });
+  }, []);
+
+  useEffect(() => {
+    setTotalPages(Math.round(donateData.length / itemsPerPage));
     if (currentPage === totalPages) {
       setIsNextDisabled(true);
     }
     if (currentPage === 1) setIsPrevDisabled(true);
-  }, [itemsPerPage, currentPage, totalPages]);
+  }, [itemsPerPage, currentPage, totalPages, donateData.length]);
 
   const nextHandler = (event) => {
     if (currentPage < totalPages) {
@@ -48,20 +59,22 @@ const Donate = () => {
           <div style={{ margin: "0 0", padding: "3rem 10%" }}>
             <div className="text-4xl font-semibold ">Pet Charity Links</div>
             <div>
-              {CharityList.slice(
-                currentPage * itemsPerPage - itemsPerPage,
-                currentPage * itemsPerPage
-              ).map((item) => {
-                return (
-                  <CharityItem
-                    key={item.id}
-                    logo={item.logo}
-                    title={item.title}
-                    url={item.url}
-                    donateLink={item.donateLink}
-                  />
-                );
-              })}
+              {donateData
+                .slice(
+                  currentPage * itemsPerPage - itemsPerPage,
+                  currentPage * itemsPerPage
+                )
+                .map((item) => {
+                  return (
+                    <CharityItem
+                      key={item.id}
+                      logo={item.logo}
+                      title={item.title}
+                      url={item.url}
+                      donateLink={item.donateLink}
+                    />
+                  );
+                })}
             </div>
             <div className="flex gap-4 justify-end -mt-8">
               <Button
