9cd880fdc02b5a4035af7db508a3c00e790cdbd3
AshKalor <akhibkalnoor@gmail.com>
2021-09-12 23:11:59 +0530

    Change event images

--- src/components/Home/EventData.js
+++ src/components/Home/EventData.js
@@ -1,25 +1,28 @@
 import eventCard1 from "../../assets/img/home/eventcard1.jpg";
 import eventCard2 from "../../assets/img/home/eventcard2.jpg";
 import eventCard3 from "../../assets/img/home/eventcard3.jpg";
+import event1 from "../../assets/img/home/event1.jpg";
+import event3 from "../../assets/img/home/event3.jpg";
+import event2 from "../../assets/img/home/event2.jpg";
 
 export const EventData = [
   {
     id: 1,
-    img: eventCard1,
+    img: event1,
     title: "NYC Adoption Fair",
     date: "3rd August at 4pm",
     location: "Washington square park",
   },
   {
     id: 2,
-    img: eventCard2,
+    img: event2,
     title: "Bronx Adoption Fair",
     date: "5th September at 4pm",
     location: "Bronx park",
   },
   {
     id: 3,
-    img: eventCard3,
+    img: event3,
     title: "Jersey Adoption Fair",
     date: "6th October at 4pm",
     location: "Jersey park",
