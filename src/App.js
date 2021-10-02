3176ef876e3398b976553885d4e917afa31c6137
AshKalor <akhibkalnoor@gmail.com>
2021-10-02 23:17:28 +0530

    Rollback to previous version that had no useWindowSize hook

--- src/App.js
+++ src/App.js
@@ -9,50 +9,52 @@ import Donate from "./pages/Donate";
 import ScrollToTop from "./components/layout/ScrollToTop";
 import Pet from "./components/Adopt/Pet";
 
-function App() {
+const App = () => {
   return (
-    <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/home" />
-        </Route>
-        <Route path="/home" exact>
-          <ScrollToTop>
-            <Home />
-          </ScrollToTop>
-        </Route>
-        <Route path="/adopt" exact>
-          <ScrollToTop>
-            <Adopt />
-          </ScrollToTop>
-        </Route>
-        <Route path="/shelter" exact>
-          <Redirect to="/shelter/:searchType" exact />
-        </Route>
-        <Route path="/shelter/:searchType" exact>
-          <Shelter />
-        </Route>
-        <Route path="/donate" exact>
-          <ScrollToTop>
-            <Donate />
-          </ScrollToTop>
-        </Route>
-        <Route path="/contact" exact>
-          <ScrollToTop>
-            <Contact />
-          </ScrollToTop>
-        </Route>
-        <Route path="/adopt/:petName">
-          <ScrollToTop>
-            <Pet />
-          </ScrollToTop>
-        </Route>
-        <Route path="*" exact>
-          <Redirect to="/home" />
-        </Route>
-      </Switch>
-    </Layout>
+    <>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home" exact>
+            <ScrollToTop>
+              <Home />
+            </ScrollToTop>
+          </Route>
+          <Route path="/adopt" exact>
+            <ScrollToTop>
+              <Adopt />
+            </ScrollToTop>
+          </Route>
+          <Route path="/shelter" exact>
+            <Redirect to="/shelter/:searchType" exact />
+          </Route>
+          <Route path="/shelter/:searchType" exact>
+            <Shelter />
+          </Route>
+          <Route path="/donate" exact>
+            <ScrollToTop>
+              <Donate />
+            </ScrollToTop>
+          </Route>
+          <Route path="/contact" exact>
+            <ScrollToTop>
+              <Contact />
+            </ScrollToTop>
+          </Route>
+          <Route path="/adopt/:petName">
+            <ScrollToTop>
+              <Pet />
+            </ScrollToTop>
+          </Route>
+          <Route path="*" exact>
+            <Redirect to="/home" />
+          </Route>
+        </Switch>
+      </Layout>
+    </>
   );
-}
+};
 
 export default App;
